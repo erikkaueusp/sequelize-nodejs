1da36a28c9a213bf461b76b87ba5075ffa933914
Erik <erikkaue@gmail.com>
2022-04-15 14:01:36 -0300

    Criada rota pessoas

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app = express()
 
-app.use(bodyParser.json())
 
 const port = 3000
 
-app.get('/teste', (req, res) => res
-    .status(200)
-    .send({ mensagem: 'Boas vindas' }))
+routes(app)
 
 app.listen(port, () => console.log(`Rodando na porta: ${port}`))
+
+module.exports = app
