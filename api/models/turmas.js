396e491dc5d7de2c4758045a83acfd82165b6650
Erik <erikkaue@gmail.com>
2022-04-15 17:42:07 -0300

    Criando mapeamentos entre entidades

--- api/models/turmas.js
+++ api/models/turmas.js
@@ -1,23 +1,28 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Turmas extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Turmas extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            Turmas.hasMany(models.Matriculas, {
+                foreignKey: 'turma_id'
+            })
+            Turmas.belongsTo(models.Pessoas)
+            Turmas.belongsTo(models.Niveis)
+
+        }
     }
-  }
-  Turmas.init({
-    data_inicio: DataTypes.DATEONLY
-  }, {
-    sequelize,
-    modelName: 'Turmas',
-  });
-  return Turmas;
+    Turmas.init({
+        data_inicio: DataTypes.DATEONLY
+    }, {
+        sequelize,
+        modelName: 'Turmas',
+    });
+    return Turmas;
 };
