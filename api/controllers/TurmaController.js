2925bbf04eb1773c4f2984ac2608c4bfaf7a28b6
Erik <erikkaue@gmail.com>
2022-04-16 14:50:27 -0300

    criado filtro para data no recurso turmas

--- api/controllers/TurmaController.js
+++ api/controllers/TurmaController.js
@@ -1,10 +1,18 @@
 const database = require('../models')
+Sequelize = require('sequelize')
+const Op = Sequelize.Op
+
 
 class TurmaController {
 
     static async getAll(req, res) {
+        const { dataInicial, dataFinal } = req.query
+        const where = {}
+        dataInicial || dataFinal ? where.data_inicio = {} : null
+        dataInicial ? where.data_inicio[Op.gte] = dataInicial : null
+        dataFinal ? where.data_inicio[Op.lte] = dataFinal : null
         try {
-            const todasAsTurmas = await database.Turmas.findAll()
+            const todasAsTurmas = await database.Turmas.findAll({ where })
             return res.status(200).json(todasAsTurmas)
         } catch (error) {
             return res.status(400).json(error.message);
